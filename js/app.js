562e90ce8c69ec8e6e7b2cea8440edcab030e26b
Nicholas D'Amico <nickalan82@icloud.com>
2017-01-19 08:54:24 -0500

    Updated Styles + lightbox custom styles

--- js/app.js
+++ js/app.js
@@ -5,8 +5,19 @@ $(document).ready(function() {
   ////////////////////////////////////////
 
   //  Both AJAX API Requests are made at page load The HTML fomatted by callback functions are then stored in these variables and called on event 'click' of navigation tab.
+
   var photoHTML = '';
   var albumHTML = '';
+  var Authorname = '';
+
+  ////////////////////////////////////////
+  //  LightBox Options
+  ////////////////////////////////////////
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  })
+
 
   ////////////////////////////////////////
   //  AJAX REQUEST FOR FLICKR PUBLIC API
@@ -15,13 +26,12 @@ $(document).ready(function() {
   var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
   var flickrData = {
-    tags  : 'mountains',
+    tags  : 'sunset',
     format  : 'json'
   };
 
   function flickrCallback(data) {
     var photoLink = '';
-
     $.each(data.items, function(i, photo) {
       if(i <= 8) {
         photoLink = photo.media.m;
@@ -31,7 +41,9 @@ $(document).ready(function() {
         //  Build anchor tag
         photoHTML += '<a href="' + photoLink + '"';
         photoHTML += ' data-lightbox="image-' + 1 + '"';
-        photoHTML += ' data-title="Photo-' + i + '">';
+        photoHTML += ' data-title="' + photo.title + '"';
+        photoHTML += ' title="' + photo.date_taken + '">';
+        // photoHTML += ' data-title="Photo-' + i + '">';
         //  Build img tag
         photoHTML += '<img src="' + photo.media.m + '"';
         photoHTML += ' alt="' + photo.title + '">';
@@ -39,6 +51,7 @@ $(document).ready(function() {
         photoHTML += '</a>';
         //  Close div tag
         photoHTML += '</div>';
+        Authorname = photo.author;
       }
     }); //  End of Loop
     $('#contentRow').html(photoHTML);
@@ -46,22 +59,24 @@ $(document).ready(function() {
 
   $.getJSON( flickrURL, flickrData, flickrCallback );
 
+
   //////////////////////////////////////////
   //  AJAX REQUEST FOR SPOTIFY PUBLIC API
   //////////////////////////////////////////
   var spotifyURL = "https://api.spotify.com/v1/search";
   var spotifyData = {
            q: 'Pink Floyd',
-           type: 'album'
+           type: 'album',
+           limit: 9
          };
          function spotifyCallback(data) {
-
+           console.log(data);
            $.each(data.albums.items, function(i, album) {
                albumHTML += '<div class="img-box sm-col-50 md-col-1-3">';
                //  Build anchor tag
                albumHTML += '<a href="' + album.images[0].url + '"';
                albumHTML += ' data-lightbox="image-' + 1 + '"';
-               albumHTML += ' data-title="Photo-' + i + '">';
+               albumHTML += ' data-title="' + album.name + '">';
                //  Build img tag
                albumHTML += '<img src="' + album.images[1].url + '"';
                albumHTML += ' alt="' + album.name + '">';
@@ -83,7 +98,9 @@ $(document).ready(function() {
 
   //  FLICKR-LINK NAV 'CLICK'
   $('.flickr-link').on('click', function(e) {
+    //  Prevent page load
     e.preventDefault();
+    //  Content replace with stored formatted HTML from API request.
     $('#contentRow').fadeOut('slow', function() {
       $(this).html(photoHTML).fadeIn('slow');
     });
@@ -98,7 +115,9 @@ $(document).ready(function() {
 
   //  SPOTIFY-LINK NAV 'CLICK'
   $('.spotify-link').on('click', function(e){
+    //  Prevent page load
     e.preventDefault();
+    //  Content replace with stored formatted HTML from API request.
     $('#contentRow').fadeOut('slow', function() {
       $(this).html(albumHTML).fadeIn('slow');
     });
