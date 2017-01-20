cab8bd35278f363064173380c7b3929192b5346c
Nicholas D'Amico <nickalan82@icloud.com>
2017-01-20 00:55:28 -0500

    Search input added & Success/Failure Methods on AJAX request

--- js/app.js
+++ js/app.js
@@ -1,134 +1,166 @@
 $(document).ready(function() {
 
-  ////////////////////////////////////////
-  //  Global Variables
-  ////////////////////////////////////////
+  //  todo.
+    //1. ** Style jqHXR.fail() msg block <span id="#failMsg">
 
-  //  Both AJAX API Requests are made at page load The HTML fomatted by callback functions are then stored in these variables and called on event 'click' of navigation tab.
+    ////////////////////////////////////////
+    //  Global Variables
+    ////////////////////////////////////////
 
-  var photoHTML = '';
-  var albumHTML = '';
-  var Authorname = '';
+    //  Both AJAX API Requests are made at page load The HTML fomatted by callback functions are then stored in these variables and called on event 'click' of navigation tab.
 
-  ////////////////////////////////////////
-  //  LightBox Options
-  ////////////////////////////////////////
-  lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true
-  })
+    var photoHTML = '';
+    var albumHTML = '';
+    var Authorname = '';
 
-
-  ////////////////////////////////////////
-  //  AJAX REQUEST FOR FLICKR PUBLIC API
-  ////////////////////////////////////////
-
-  var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-
-  var flickrData = {
-    tags  : 'sunset',
-    format  : 'json'
-  };
-
-  function flickrCallback(data) {
-    var photoLink = '';
-    $.each(data.items, function(i, photo) {
-      if(i <= 8) {
-        photoLink = photo.media.m;
-        photoLink = photoLink.replace('_m', '');
-        // photoLink = photoLink.text().replace('_m', '');
-        photoHTML += '<div class="img-box sm-col-50 md-col-1-3">';
-        //  Build anchor tag
-        photoHTML += '<a href="' + photoLink + '"';
-        photoHTML += ' data-lightbox="image-' + 1 + '"';
-        photoHTML += ' data-title="' + photo.title + '"';
-        photoHTML += ' title="' + photo.date_taken + '">';
-        // photoHTML += ' data-title="Photo-' + i + '">';
-        //  Build img tag
-        photoHTML += '<img src="' + photo.media.m + '"';
-        photoHTML += ' alt="' + photo.title + '">';
-        //  Close a tag
-        photoHTML += '</a>';
-        //  Close div tag
-        photoHTML += '</div>';
-        Authorname = photo.author;
-      }
-    }); //  End of Loop
-    $('#contentRow').html(photoHTML);
-  };  // End of Function
-
-  $.getJSON( flickrURL, flickrData, flickrCallback );
+    //  AJAX status messages for, Done(successful), Fail(not successful), Always(success or failure);
+    var doneMsg = 'Request successfully completed';
+    var failMsg = 'Sorry, Request did not complete';
+    var alwaysMsg = 'Request Status : ';
 
 
-  //////////////////////////////////////////
-  //  AJAX REQUEST FOR SPOTIFY PUBLIC API
-  //////////////////////////////////////////
-  var spotifyURL = "https://api.spotify.com/v1/search";
-  var spotifyData = {
-           q: 'Pink Floyd',
-           type: 'album',
-           limit: 9
-         };
-         function spotifyCallback(data) {
-           console.log(data);
-           $.each(data.albums.items, function(i, album) {
-               albumHTML += '<div class="img-box sm-col-50 md-col-1-3">';
-               //  Build anchor tag
-               albumHTML += '<a href="' + album.images[0].url + '"';
-               albumHTML += ' data-lightbox="image-' + 1 + '"';
-               albumHTML += ' data-title="' + album.name + '">';
-               //  Build img tag
-               albumHTML += '<img src="' + album.images[1].url + '"';
-               albumHTML += ' alt="' + album.name + '">';
-               //  Close a tag
-               albumHTML += '</a>';
-               //  Close div tag
-               albumHTML += '</div>';
-           }); //  End of Loop
-          //  $('#contentRow').html(albumHTML);
-         };  // End of Function
+    ////////////////////////////////////////
+    //  LightBox Options
+    ////////////////////////////////////////
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    })
 
 
-  $.getJSON( spotifyURL, spotifyData, spotifyCallback );
+    ////////////////////////////////////////
+    //  AJAX REQUEST FOR FLICKR PUBLIC API
+    ////////////////////////////////////////
+
+    //  URL for AJAX JSON Request
+    var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+    //  DATA for AJAX Request
+    var flickrData = {
+        tags: 'sunset',
+        format: 'json'
+    };
+
+    //  Callback for AJAX Request
+    function flickrCallback(data) {
+        var photoLink = '';
+        $.each(data.items, function(i, photo) {
+            if (i <= 8) {
+                photoLink = photo.media.m;
+                photoLink = photoLink.replace('_m', '');
+                // photoLink = photoLink.text().replace('_m', '');
+                photoHTML += '<div class="img-box sm-col-50 md-col-1-3">';
+                //  Build anchor tag
+                photoHTML += '<a href="' + photoLink + '"';
+                photoHTML += ' data-lightbox="image-' + 1 + '"';
+                photoHTML += ' data-title="' + photo.title + '"';
+                photoHTML += ' title="' + photo.date_taken + '">';
+                // photoHTML += ' data-title="Photo-' + i + '">';
+                //  Build img tag
+                photoHTML += '<img src="' + photo.media.m + '"';
+                photoHTML += ' alt="' + photo.title + '">';
+                //  Close a tag
+                photoHTML += '</a>';
+                //  Close div tag
+                photoHTML += '</div>';
+                Authorname = photo.author;
+            }
+        }); //  End of Loop
+        $('#contentRow').html(photoHTML);
+    }; // End of Function
+
+    //  .getJSON 'GET' Request returns JSON format
+    $.getJSON(flickrURL, flickrData, flickrCallback)
+       .done(console.log(doneMsg))
+       .fail('<span id="#failMSG">' + failMSG + '</span>')
+       .always(console.log(alwaysMsg + jqXHR.responseText));
 
 
-  ////////////////////////////////////////
-  //  EVENT HANDLERS
-  ////////////////////////////////////////
+    //////////////////////////////////////////
+    //  AJAX REQUEST FOR SPOTIFY PUBLIC API
+    //////////////////////////////////////////
 
-  //  FLICKR-LINK NAV 'CLICK'
-  $('.flickr-link').on('click', function(e) {
-    //  Prevent page load
-    e.preventDefault();
-    //  Content replace with stored formatted HTML from API request.
-    $('#contentRow').fadeOut('slow', function() {
-      $(this).html(photoHTML).fadeIn('slow');
+    //  URL for AJAX JSON Request
+    var spotifyURL = "https://api.spotify.com/v1/search";
+
+    //  DATA for AJAX Request
+    var spotifyData = {
+        q: 'Pink Floyd',
+        type: 'album',
+        limit: 9
+    };
+
+    //  Callback for AJAX Request
+    function spotifyCallback(data) {
+        console.log(data);
+        $.each(data.albums.items, function(i, album) {
+            albumHTML += '<div class="img-box sm-col-50 md-col-1-3">';
+            //  Build anchor tag
+            albumHTML += '<a href="' + album.images[0].url + '"';
+            albumHTML += ' data-lightbox="image-' + 1 + '"';
+            albumHTML += ' data-title="' + album.name + '">';
+            //  Build img tag
+            albumHTML += '<img src="' + album.images[1].url + '"';
+            albumHTML += ' alt="' + album.name + '">';
+            //  Close a tag
+            albumHTML += '</a>';
+            //  Close div tag
+            albumHTML += '</div>';
+        }); //  End of Loop
+        //  $('#contentRow').html(albumHTML);
+    }; // End of Function
+
+    //  .getJSON 'GET' Request returns JSON format
+    $.getJSON(spotifyURL, spotifyData, spotifyCallback);
+        .done(console.log(doneMsg))
+        .fail('<span id="#failMSG">' + failMSG + '</span>')
+        .always(console.log(alwaysMsg + jqXHR.responseText));
+
+
+    ////////////////////////////////////////
+    //  EVENT HANDLERS
+    ////////////////////////////////////////
+
+    //  FLICKR-LINK NAV 'CLICK'
+    $('.flickr-link').on('click', function(e) {
+        //  Prevent page load
+        e.preventDefault();
+        //  Content replace with stored formatted HTML from API request.
+        $('#contentRow').fadeOut('slow', function() {
+            $(this).html(photoHTML)
+                .fadeIn('slow');
+        });
+        //  Hide Title, Update Title, then Show Title
+        $('.main-title').fadeOut('slow', function() {
+            $(this).html('Flickr API Photo Feed');
+        }).fadeIn('slow');
+
+        $('#main-content').fadeOut('slow', function() {
+            $(this).removeClass('spotify-main')
+                .addClass('flickr-main')
+                .fadeIn('slow');
+        });
     });
-    //  Hide Title, Update Title, then Show Title
-    $('.main-title').fadeOut('slow',function(){
-      $(this).html('Flickr API Photo Feed');
-    }).fadeIn('slow');
-    $('#main-content').fadeOut('slow',function(){
-      $(this).removeClass('spotify-main').addClass('flickr-main').fadeIn('slow');
-    });
-  });
 
-  //  SPOTIFY-LINK NAV 'CLICK'
-  $('.spotify-link').on('click', function(e){
-    //  Prevent page load
-    e.preventDefault();
-    //  Content replace with stored formatted HTML from API request.
-    $('#contentRow').fadeOut('slow', function() {
-      $(this).html(albumHTML).fadeIn('slow');
+    //  SPOTIFY-LINK NAV 'CLICK'
+    $('.spotify-link').on('click', function(e) {
+        //  Prevent page load
+        e.preventDefault();
+        //  Content replace with stored formatted HTML from API request.
+        $('#contentRow').fadeOut('slow', function() {
+            $(this).html(albumHTML).fadeIn('slow');
+        });
+        //  Hide Title, Update Title, then Show Title
+        $('.main-title').fadeOut('slow', function() {
+            $(this).html('Spotify API Album Feed');
+        }).fadeIn('slow');
+
+        $('#main-content').fadeOut('slow', function() {
+            $(this).removeClass('flickr-main')
+                .addClass('spotify-main')
+                .fadeIn('slow');
+        });
     });
-    //  Hide Title, Update Title, then Show Title
-    $('.main-title').fadeOut('slow', function() {
-      $(this).html('Spotify API Album Feed');
-    }).fadeIn('slow');
-    $('#main-content').fadeOut('slow',function(){
-      $(this).removeClass('flickr-main').addClass('spotify-main').fadeIn('slow');
-    });
-  });
 
 
 }); //  END of ready
